4edad2469527b97ec0b0b5dbaaf1b4b7ea577f84
Eti Youngreis <156570766+eti-youngreis@users.noreply.github.com>
2024-09-30 10:51:11 +0300

    Rename variable is_want_to_play to want_to_play for clarity in main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from game import Game
 
 
 def main():
-    is_want_to_play = int(input("Enter 1 to play, else 0"))
-    while is_want_to_play:
+    want_to_play = int(input("Enter 1 to play, else 0"))
+    while want_to_play:
         game = Game()
-        is_want_to_play = int(input("Enter 1 to play, else 0"))
+        want_to_play = int(input("Enter 1 to play, else 0"))
 
 
 if __name__ == '__main__':
